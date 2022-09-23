b2385a1bbf7e47644eeb1d4c2abc617aedd1fefb
tuffkidd-imac24 <tuffkidd@gmail.com>
2022-09-23 17:32:44 +0900

    미디어라이브러리

--- wp-content/themes/LSNewsroom/theme-parts/medialibrary-albums.php
+++ wp-content/themes/LSNewsroom/theme-parts/medialibrary-albums.php
@@ -32,45 +32,36 @@ $albums = $Frontend->get_albums($paged, $taxo);
 				<a href="<?php echo site_url($lib . '/photostream'); ?>" class="cj-button button-default <?php if (get_query_var($lb) == 'photostream' || get_query_var('heritagelib' == 'photostream')) { ?>selected<?php } ?>">이미지</a>
 				<a href="<?php echo site_url($lib . '/albums'); ?>" class="cj-button button-default <?php if (get_query_var($lb) == 'albums' || get_query_var('heritagelib') == 'albums') { ?>selected<?php } ?>">영상</a>
 			</div>
+
+			<ul class="albums">
+				<?php
+				if ($albums) :
+					foreach ($albums as $album) :
+						// count
+						$medias = $Frontend->get_medias($album->term_id, 1, -1, $taxo, $post_type)->posts;
+						$total_posts = count($medias);
+						$img = wp_get_attachment_image_src(attachment_url_to_postid(get_term_meta($album->term_id, "album_img", TRUE)), 'category-list-header-thumb');
+				?>
+						<li>
+							<a href="<?php echo get_category_link($album->term_id); ?>">
+								<span class="album-photo-count">
+									<img src="<?php echo THEME_IMAGE_URI . '/icon-photo-count.png'; ?>" alt="미디어 수 아이콘"> <?php echo number_format($total_posts); ?>
+								</span>
+								<div class="album-thumb-wrap">
+									<img src="<?php echo $img[0]; ?>" alt="<?php echo $album->name; ?> 앨범 대표 이미지">
+								</div>
+								<div class="album-gradient-overlay"></div>
+								<span class="album-title"><?php echo $album->name; ?></span>
+							</a>
+						</li>
+				<?php
+					endforeach;
+				endif;
+				?>
+			</ul>
 		</div>
 	</div>
 </section>
-
-<div class="container">
-	<div class="viewtype">
-		<a href="<?php echo site_url($lib . '/albums'); ?>" class="cj-button button-default <?php if (get_query_var($lb) == 'albums' || get_query_var('heritagelib') == 'albums') { ?>selected<?php } ?>">앨범</a>
-		<a href="<?php echo site_url($lib . '/photostream'); ?>" class="cj-button button-default <?php if (get_query_var($lb) == 'photostream' || get_query_var('heritagelib' == 'photostream')) { ?>selected<?php } ?>">포토스트림</a>
-	</div>
-	<!--
-	<h4 class="medialib-title"><?php if (get_query_var($lb) == 'albums' || get_query_var('heritagelib') == 'albums') { ?>앨범(선택됨)<?php } else { ?>포토스트림(선택됨)<?php } ?></h4>
-	-->
-	<ul class="albums">
-		<?php
-		if ($albums) :
-			foreach ($albums as $album) :
-				// count
-				$medias = $Frontend->get_medias($album->term_id, 1, -1, $taxo, $post_type)->posts;
-				$total_posts = count($medias);
-				$img = wp_get_attachment_image_src(attachment_url_to_postid(get_term_meta($album->term_id, "album_img", TRUE)), 'category-list-header-thumb');
-		?>
-				<li>
-					<a href="<?php echo get_category_link($album->term_id); ?>">
-						<span class="album-photo-count">
-							<img src="<?php echo THEME_IMAGE_URI . '/icon-photo-count.png'; ?>" alt="미디어 수 아이콘"> <?php echo number_format($total_posts); ?>
-						</span>
-						<div class="album-thumb-wrap">
-							<img src="<?php echo $img[0]; ?>" alt="<?php echo $album->name; ?> 앨범 대표 이미지">
-						</div>
-						<div class="album-gradient-overlay"></div>
-						<span class="album-title"><?php echo $album->name; ?></span>
-					</a>
-				</li>
-		<?php
-			endforeach;
-		endif;
-		?>
-	</ul>
-</div>
 <!-- index -->
 <?php
 get_footer();
